1d69087b329f6a971598cbce9aab52b511bfbff2
Gianni Declercq <gianni.declercq@korazon.be>
2024-01-04 09:13:45 +0100

    update UmbracoMustBeTrueAttribute

--- srcProject/Attributes/UmbracoMustBeTrueAttribute.cs
+++ srcProject/Attributes/UmbracoMustBeTrueAttribute.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿#if NET || NETCOREAPP
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+#else
+using System.Web.Mvc;
+using System.Collections.Generic;
+#endif
+using System;
 using System.ComponentModel.DataAnnotations;
 using Umbraco.DataAnnotations.Interfaces;
 
@@ -8,7 +14,11 @@ namespace Umbraco.DataAnnotations.Attributes
     /// Specifies that the property must be true.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-    public sealed class UmbracoMustBeTrueAttribute : ValidationAttribute, IUmbracoValidationAttribute
+#if NET || NETCOREAPP
+    public sealed class UmbracoMustBeTrueAttribute : ValidationAttribute, IUmbracoValidationAttribute, IClientModelValidator
+#else
+    public sealed class UmbracoMustBeTrueAttribute : ValidationAttribute, IUmbracoValidationAttribute, IClientValidatable
+#endif
     {
         public string DictionaryKey { get; set; } = "MustBeTrueError";
 
@@ -26,5 +36,32 @@ namespace Umbraco.DataAnnotations.Attributes
             ErrorMessage = UmbracoDictionary.GetDictionaryValue(DictionaryKey);
             return base.FormatErrorMessage(name);
         }
+
+#if NET || NETCOREAPP
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var errorMessage = UmbracoDictionary.GetDictionaryValue(DictionaryKey);
+
+            context.Attributes.Add("data-val-mustbetrue", errorMessage); // Add attributes for client-side validation
+            context.Attributes.Add("data-val", "true");
+        }
+#else
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            ErrorMessage = UmbracoDictionary.GetDictionaryValue(DictionaryKey);
+            var rule = new ModelClientValidationRule()
+            {
+                ValidationType = "mustbetrue",
+                ErrorMessage = FormatErrorMessage(metadata.GetDisplayName()),
+            };
+
+            yield return rule;
+        }
+#endif
     }
 }
